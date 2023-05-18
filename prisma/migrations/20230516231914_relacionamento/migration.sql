ec81355fe116a285ef0f5d2375e9673f6704ca71
marcoscl84 <marcoscontelima@yahoo.com.br>
2023-05-18 19:46:54 -0300

    feat: configurações multer e prisma

--- prisma/migrations/20230516231914_relacionamento/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE `fotoVeiculo` ADD CONSTRAINT `fotoVeiculo_veiculoId_fkey` FOREIGN KEY (`veiculoId`) REFERENCES `veiculo`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/migrations/20230518202525_/migration.sql
+++ prisma/migrations/20230518202525_/migration.sql
@@ -1,23 +1,26 @@
 -- CreateTable
 CREATE TABLE `veiculo` (
-    `id` VARCHAR(191) NOT NULL,
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
     `placa` VARCHAR(7) NOT NULL DEFAULT '',
     `rastrado` BOOLEAN NOT NULL DEFAULT false,
     `comprimento` DOUBLE NOT NULL DEFAULT 0.00,
     `largura` DOUBLE NOT NULL DEFAULT 0.00,
     `altura` DOUBLE NOT NULL DEFAULT 0.00,
     `cubagem` DOUBLE NOT NULL DEFAULT 0.00,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `created_at` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `fotoVeiculo` (
-    `id` VARCHAR(191) NOT NULL,
-    `veiculoId` VARCHAR(191) NOT NULL,
-    `image` LONGBLOB NOT NULL,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `file` LONGBLOB NOT NULL,
+    `created_at` DATETIME(3) NOT NULL,
+    `veiculoId` INTEGER NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `fotoVeiculo` ADD CONSTRAINT `fotoVeiculo_veiculoId_fkey` FOREIGN KEY (`veiculoId`) REFERENCES `veiculo`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
